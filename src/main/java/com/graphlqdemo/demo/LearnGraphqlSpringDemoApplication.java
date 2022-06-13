ae74f6ffd1f4b7493bc063efdfdd43b747025a41
HbotondS <hbotond97@gmail.com>
2022-06-13 20:02:35 +0300

    add initial springboot project

---
+++ src/main/java/com/graphlqdemo/demo/LearnGraphqlSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.graphlqdemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGraphqlSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGraphqlSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graphlqdemo/demo/LearnGraphqlSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphlqdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGraphqlSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
