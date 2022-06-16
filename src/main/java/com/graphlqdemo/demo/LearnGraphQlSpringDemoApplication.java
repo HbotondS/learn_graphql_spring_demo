559ef1a900fee86ff0091ca5431029a938b3ef0f
HbotondS <hbotond97@gmail.com>
2022-06-16 19:15:46 +0300

    fixed typo in class name

--- src/main/java/com/graphlqdemo/demo/LearnGraphQlSpringDemoApplication.java
+++ src/main/java/com/graphlqdemo/demo/LearnGraphQlSpringDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnGraphqlSpringDemoApplication {
+public class LearnGraphQlSpringDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnGraphqlSpringDemoApplication.class, args);
+        SpringApplication.run(LearnGraphQlSpringDemoApplication.class, args);
     }
 
 }
